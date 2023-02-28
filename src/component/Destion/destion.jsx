764312049ae8a4ab31d782c58644544e8593bc70
Aditya vardhan singh <93644476+Adisaum642@users.noreply.github.com>
2023-02-28 04:17:11 +0000

    update

--- src/component/Destion/destion.jsx
+++ src/component/Destion/destion.jsx
@@ -9,11 +9,11 @@ import Tuscany from "../../utility/TuscanyImage.png"
 import "./destion.css"
 
 const Item = styled(Paper)(({ theme }) => ({
-//   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-//   ...theme.typography.body2,
-//   padding: theme.spacing(1),
-//   textAlign: 'center',
-//   color: theme.palette.text.secondary,
+  //   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  //   ...theme.typography.body2,
+  //   padding: theme.spacing(1),
+  //   textAlign: 'center',
+  //   color: theme.palette.text.secondary,
 }));
 
 export default function Destion() {
@@ -21,16 +21,16 @@ export default function Destion() {
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={3}>
         <Grid item xs="auto" className='paris'>
-          <Item><img src={Paris} alt="Paris"/><div className='title-paris'>Paris</div ><div ><span className='from'>From</span> <br/> <span className="price-paris">$600</span></div></Item>
+          <Item><img src={Paris} alt="Paris" /><div className='title-paris'>Paris</div ><div ><span className='from'>From</span> <br /> <span className="price-paris">$600</span></div></Item>
         </Grid>
         <Grid item xs="auto" className='greece'>
-          <Item ><img src={Greece} alt="Greece"/><div className='title-greece'>Greece</div ><div ><span className='from'>From</span> <br/> <span className="price-paris">$600</span></div></Item>
+          <Item ><img src={Greece} alt="Greece" /><div className='title-greece'>Greece</div ><div ><span className='from'>From</span> <br /> <span className="price-paris">$600</span></div></Item>
         </Grid>
         <Grid item xs="auto" className='norway'>
-          <Item><img src={Norway} alt="Norway"/><div className='title-paris'>Paris</div ><div ><span className='from'>From</span> <br/> <span className="price-paris">$600</span></div></Item>
+          <Item><img src={Norway} alt="Norway" /><div className='title-paris'>Norway</div ><div ><span className='from'>From</span> <br /> <span className="price-paris">$895</span></div></Item>
         </Grid>
         <Grid item xs="auto" className='tuscany'>
-          <Item><img src={Tuscany} alt="Tuscany"/><div className='title-paris'>Paris</div ><div ><span className='from'>From</span> <br/> <span className="price-paris">$600</span></div></Item>
+          <Item><img src={Tuscany} alt="Tuscany" /><div className='title-paris'>Tuscany</div ><div ><span className='from'>From</span> <br /> <span className="price-paris">$1245</span></div></Item>
         </Grid>
       </Grid>
     </Box>
