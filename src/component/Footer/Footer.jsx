491dbe4c38630d2b69b7926f65ae216ab537f5e7
Aditya vardhan singh <93644476+Adisaum642@users.noreply.github.com>
2023-02-28 04:10:53 +0000

    footer update

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -7,31 +7,31 @@ export default function Footer() {
 
 
 
-    return (
-        <div className="footer">
-            <div className="desc">
-                <div  className="logo"><img src={Logo} alt="logo" /></div>
-                <div className="paragraph">Fickle Flight is your one-stop travel portal. We offer hassle free flight and hotel bookings. We also have all your flight needs in you online shop.</div>
-<div><span className="fb"><img src={Fb} alt="fb"/></span><span className="insta"><img src={Insta} alt="insta.logo"/></span><span className="twit"><img src={Twit} alt="twitter"/></span></div>
+  return (
+    <div className="footer">
+      <div className="desc">
+        <div className="logo"><img src={Logo} alt="logo" /></div>
+        <div className="paragraph">Fickle Flight is your one-stop travel portal. We offer hassle free flight and hotel bookings. We also have all your flight needs in you online shop.</div>
+        <div><span className="fb"><img src={Fb} alt="fb" /></span><span className="insta"><img src={Insta} alt="insta.logo" /></span><span className="twit"><img src={Twit} alt="twitter" /></span></div>
 
-            </div>
-<div className="contant">
-<ul>
-<div className="company">Company</div>
-<li>About us</li>
-<li>News</li>
-<li>Career</li>
-<li>How we are work</li>
+      </div>
+      <div className="contant">
+        <ul >
+          <div className="company">Company</div>
+          <div className="box"><li>About us</li>
+            <li>News</li>
+            <li>Career</li>
+            <li>How we are work</li></div>
 
-</ul>
-
-
-</div>
+        </ul>
 
 
+      </div>
 
-        </div>
-    )
+
+
+    </div>
+  )
 
 
 }
